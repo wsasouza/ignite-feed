70e0c6daa1b5cffb61e965f52dda76514c0bad18
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-06-04 19:21:07 -0300

    Docs: Create Readme

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import styles from './Header.module.css';
 export function Header() {
   return (
     <header className={styles.header}>
-      <img src={igniteLogo} alt="logotipo do Ignite" />
+      <img src={igniteLogo} alt="logotipo do Ignite Rocketseat" />
       <h1>Ignite Feed</h1>
     </header>
   );
