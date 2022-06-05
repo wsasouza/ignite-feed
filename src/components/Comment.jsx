c43d14a027eb447981fba27f0aeb08ff9fa7327f
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-06-05 19:35:45 -0300

    Create Comment Estrutura

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/wsasouza.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Walter Souza</strong>
+              <time title="03 de junho às 17:00" dateTime="2022-06-03 17:00:00">
+                Cerca de 1h atrás
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+
+          <p>Muito bom Walter, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp size={20} />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
