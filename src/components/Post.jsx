c302510f5094b980bdf4b9f75078e0b7bbda2352
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-06-06 20:19:55 -0300

    Posts interaction

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -2,6 +2,48 @@ import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
+//author: {avatar_url:"", name:"", role:""}
+// publishedAt: Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/wsasouza.png',
+      name: 'Walter Santos de Andrade Souza',
+      role: 'Fullstack Developer',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'link', content: '👉 jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-06-06 20:13:42'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Walter Santos de Andrade Souza',
+      role: 'CTO Rocketseat',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content: 'Acabei de atualizar a trilha ReactJS do Ignite 🚀',
+      },
+      { type: 'link', content: '👉 app.rocketseat/ignite' },
+    ],
+    publishedAt: new Date('2022-06-03 14:15:22'),
+  },
+];
+
 export function Post() {
   return (
     <article className={styles.post}>
