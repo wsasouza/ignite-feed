2c20e3b6ccf7f2f1efe4a8672569df3056076457
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-06-05 23:12:31 -0300

    Create Component Avatar

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,15 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
 export function Comment() {
   return (
     <div className={styles.comment}>
-      <img src="https://github.com/wsasouza.png" alt="" />
+      <Avatar
+        src="https://github.com/wsasouza.png"
+        alt="foto do usuário"
+        hasBorder={false}
+      />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { PencilLine } from 'phosphor-react';
+import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
 
@@ -11,11 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/wsasouza.png"
-          alt="foto do usuário"
-        />
+        <Avatar src="https://github.com/wsasouza.png" alt="foto do usuário" />
         <strong>Walter Souza</strong>
         <span>Full Stack Developer</span>
       </div>
