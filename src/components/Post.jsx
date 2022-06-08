68bc7f5935203e9d81dc7660f7d81e35ab621faf
Walter Santos de Andrade Souza <wsasouza@hotmail.com>
2022-06-08 10:01:51 -0300

    Validate Form

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -32,7 +32,12 @@ export function Post({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange(event) {
+    event.target.setCustomValidity('');
     setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid(event) {
+    event.target.setCustomValidity('Esse campo é obrigatório!');
   }
 
   function deleteComment(commentToDelete) {
@@ -41,6 +46,8 @@ export function Post({ author, publishedAt, content }) {
     });
     setComments(commentsWithoutDeletedOne);
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -83,10 +90,14 @@ export function Post({ author, publishedAt, content }) {
           placeholder="Deixe o seu comentário"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button disabled={isNewCommentEmpty} type="submit">
+            Publicar
+          </button>
         </footer>
       </form>
 
